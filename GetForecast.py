b1b782e21b6783076ff19bda920bf9a341a3ce98
Petricia <PETERII2014@yandex.ru>
2024-06-11 09:29:52 +0300

    Привязано получение данных к интерфейсу

--- GetForecast.py
+++ GetForecast.py
@@ -2,6 +2,18 @@ from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtCore import QDate, QRegExp
 from PyQt5.QtWidgets import QMessageBox, qApp
 from PyQt5.QtGui import QRegExpValidator, QIcon
+import json
+import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+
+YANDEX_KEY = str(os.getenv('YANDEX_API_KEY'))
+OPEN_WEATHER_KEY = str(os.getenv('OPEN_WEATHER_API_KEY'))
+WEATHER_API_KEY = str(os.getenv('WEATHER_API_KEY'))
 
 
 class Ui_MainWindow(object):
@@ -35,7 +47,7 @@ class Ui_MainWindow(object):
         self.gridLayout.setObjectName("gridLayout")
         self.LELng = QtWidgets.QLineEdit(self.centralwidget)
 
-        reg_exp = QRegExp("[0-9,]*")                                        # только числа и запятая на ввод координат
+        reg_exp = QRegExp("[0-9.]*")                                        # только числа и точка на ввод координат
         validator = QRegExpValidator(reg_exp)
         self.LELng.setValidator(validator)
 
@@ -71,7 +83,7 @@ class Ui_MainWindow(object):
         self.LELat.setObjectName("LELat")
         self.LELat.setStyleSheet("selection-background-color: rgb(73, 140, 81)")
 
-        reg_exp = QRegExp("[0-9,]*")                                        # только числа и запятая на ввод координат
+        reg_exp = QRegExp("[0-9.]*")                                        # только числа и точка на ввод координат
         validator = QRegExpValidator(reg_exp)
         self.LELat.setValidator(validator)
 
@@ -155,6 +167,13 @@ class Ui_MainWindow(object):
 
             error.exec()
 
+    def clear_cord(self, cord):
+        if cord == "Lat":
+            self.LELat.setText("")
+        else:
+            self.LELng.setText("")  
+
+
     def NoCord(self, cord):
         error = QMessageBox()
         error.setWindowTitle("Некорректные координаты")
@@ -186,30 +205,195 @@ class Ui_MainWindow(object):
         error.setStandardButtons(QMessageBox.Ok)
         error.exec()
 
+    def api(self, response):
+        if response.status_code == 200:
+            return response.json()
+        else:
+            self.cant_do_request(response.status_code)
+            return
+        
+    def cant_do_request(self, e, webs):
+        error = QMessageBox()
+        error.setWindowTitle("Ошибка выполнения запроса")
+        error.setText(f"Запрос на {webs} не может быть выполнен")
+        error.setInformativeText(f"Ошибка получения запроса: {e}")
+        error.setIcon(QMessageBox.Warning)
+        error.setStandardButtons(QMessageBox.Ok)
+        error.exec()
+    
+    def cant_get_data(self, webs):
+        error = QMessageBox()
+        error.setWindowTitle("Ошибка получения данных")
+        error.setText(f"Данные c {webs} не могут быть получены")
+        error.setInformativeText("Попробуйте запросить данные ещё раз позже")
+        error.setIcon(QMessageBox.Warning)
+        error.setStandardButtons(QMessageBox.Ok)
+        error.exec()
+
+    def succ_get(self, webs):
+        succ = QMessageBox()
+        succ.setWindowTitle("Запрос выполнен")
+        succ.setText(f"Данные c {webs} успешно получены")
+        succ.setIcon(QMessageBox.information)
+        succ.setStandardButtons(QMessageBox.Ok)
+        succ.exec()
+
+    def yandex(self, KEY, name, days, lat, lon):
+        access_key = KEY
+        headers = {
+            "X-Yandex-API-Key": access_key
+        }
+        query = """{
+        weatherByPoint(request: {""" + f'lat:{lat}, lon:{lon}' + """ }) {
+        forecast {
+            days(limit:""" + str(days) + """) {
+            time
+            parts {
+                morning {
+                avgTemperature
+                windSpeed
+                pressure
+                humidity
+                }
+                day {
+                avgTemperature
+                windSpeed
+                pressure
+                humidity
+                }
+                evening {
+                avgTemperature
+                windSpeed
+                pressure
+                humidity
+                }
+                night {
+                avgTemperature
+                windSpeed
+                pressure
+                humidity
+                }
+            }
+            }
+        }
+        }
+    }"""
+        try:
+            response = self.api(
+                requests.post('https://api.weather.yandex.ru/graphql/query', headers=headers, json={'query': query}))
+            if (data := response):
+                df = pd.DataFrame(columns=['temp', 'wind', 'pressure', 'humidity'])
+                for day in data['data']['weatherByPoint']['forecast']['days']:
+                    for time in day['parts']:
+                        df.loc[day['time'][0:10:] + time] = [day['parts'][time]['avgTemperature'],
+                                                            day['parts'][time]['windSpeed'],
+                                                            day['parts'][time]['pressure'],
+                                                            day['parts'][time]['humidity']]
+                df.to_csv(f'{name}.csv', sep=';', index_label="time")
+            else:
+                self.cant_get_data("YandexWeather")
+        except Exception as e:
+            self.cant_do_request(e, "YanexWeather")
+
+
+    def open_weather(self, KEY, name, days, lat, lon, ):
+        params = {'lat': lat, 'lon': lon, 'APPID': KEY, 'cnt': days, 'units': 'metric'}
+        url = 'https://pro.openweathermap.org/data/2.5/forecast/climate'
+        try:
+            response = self.api(requests.get(url, params=params))
+            if (data := response):
+                df3 = pd.DataFrame(columns=['temp', 'wind', 'pressure', 'humidity'])
+                for day in data['list']:
+                    for time in ['morn', 'day', 'eve', 'night']:
+                        df3.loc[str(datetime.fromtimestamp(day['dt'])) + time] = [day['temp'][time], day['speed'],
+                                                                                day['pressure'],
+                                                                                day['humidity']]
+                df3.to_csv(f'{name}.csv', sep=';', index_label="time")
+            else:
+                self.cant_get_data("OpenWeather")
+        except Exception as e:
+            self.cant_do_request(e, "OpenWeather")
+
+
+    def weather_api(self, KEY, name, days, lat, lon):
+        url = 'http://api.weatherapi.com/v1/forecast.json'
+        params = {'q': f'{lat},{lon}', 'KEY': KEY, 'days': days, 'wind100kph': 'yes'}
+        try:
+            response = self.api(requests.get(url, params=params))
+            if (data := response):
+                def value(parametr, start, list):
+                    sum = 0
+                    for dict in list[start:start + 6]:
+                        sum += dict[parametr]
+                    return round(sum / 6, ndigits=2)
+
+                df2 = pd.DataFrame(columns=['temp', 'wind', 'pressure', 'humidity'])
+                for day in data['forecast']['forecastday']:
+                    n = 0
+                    for time in ['morning', 'day', 'evening', 'night']:
+                        df2.loc[time + day['date']] = [value('temp_c', n, day['hour']), value('wind_kph', n, day['hour']),
+                                                    value('pressure_in', n, day['hour']) * 25.4,
+                                                    value('humidity', n, day['hour'])]
+                        n += 6
+                df2.to_csv(f'{name}.csv', sep=';', index_label="time")
+            else:
+                self.cant_get_data("WeatherAPI")
+        except Exception as e:
+            self.cant_do_request(e, "WeatherAPI")
+
+    def date_to_str(self, date):
+        if date < 10:
+            return "0" + str(date)
+        return str(date)
+
+    def do_folder_name(self, day, mon, year, lday, lmon, lyear, lat, lng):
+        day_s = self.date_to_str(day)
+        mon_s = self.date_to_str(mon)
+        year_s = self.date_to_str(year)
+        lday_s = self.date_to_str(lday)
+        lmon_s = self.date_to_str(lmon)
+        lyear_s = self.date_to_str(lyear)
+        
+        lat_s = f"{lat:.6f}"
+        lng_s = f"{lng:.6f}"
+
+        return day_s + '.' + mon_s + '.' + year_s + "-" + lday_s + '.' + lmon_s + '.' + lyear_s + '_' + lat_s + '.' + lng_s
+
     def getting(self):
         try:
             Lat = float(str(self.LELat.text()))
-            try:
-                Lng = float(str(self.LELng.text()))
-                if Lat <= 180 and Lat >= -180:
-                    if Lng <= 90 and Lng >= -90:
-                        pass                                                                            # тут выполняются запросы
-                    else:
-                        self.NoCord("Lng")
-                else:
-                    self.NoCord("Lat")
-            except:
-                self.nonumbers("Lng")
         except:
             self.nonumbers("Lat")
+            return
+        try:
+            Lng = float(str(self.LELng.text()))
+        except:
+            self.nonumbers("Lng")
+            return 
+        if Lat <= 180 and Lat >= -180:
+            if Lng <= 90 and Lng >= -90:
+                days = QDate.currentDate().daysTo(self.CalChoose.selectedDate())
+                cur_day = QDate.currentDate().day()
+                cur_mon = QDate.currentDate().month()
+                cur_year = QDate.currentDate().year()
+                last_day = self.selected_date.day()
+                last_mon = self.selected_date.month()
+                last_year = self.selected_date.year()
 
-        
+                folder_name = self.do_folder_name(cur_day, cur_mon, cur_year, last_day, last_mon, last_year, Lat, Lng)
+                path = f"data/{folder_name}"
+                os.makedirs(path)
 
-    def clear_cord(self, cord):
-        if cord == "Lat":
-            self.LELat.setText("")
+                self.open_weather(OPEN_WEATHER_KEY, path + "/OpenWather",  days, str(Lat), str(Lng) )
+                self.weather_api(WEATHER_API_KEY, path + "/WeatherAPI", days, str(Lat), str(Lng))
+                self.yandex(YANDEX_KEY, path + "/YandexWeather", days, str(Lat), str(Lng))
+
+            else:
+                self.NoCord("Lng")
         else:
-            self.LELng.setText("")  
+            self.NoCord("Lat")
+            
+        
 
 
 
